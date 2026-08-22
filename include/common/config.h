7be8bc0ea4a4b041dc8f616330d2616654e5ae23
skv-1180 <skv.iitroorkee@gmail.com>
2026-08-22 13:15:57 +0000

    flat map orderbook implementation

--- include/common/config.h
+++ include/common/config.h
@@ -5,4 +5,5 @@ namespace config {
     inline constexpr std::size_t CACHE_LINE_SIZE = 64;
     inline constexpr std::size_t INITIAL_ORDER_CAPACITY = 5'000'000;
     inline constexpr std::size_t MAX_MARKET_SYMBOLS = 65'536;
+    inline constexpr std::size_t EXPECTED_BOOK_DEPTH = 32;
 }
